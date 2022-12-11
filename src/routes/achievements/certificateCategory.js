ee4d889b077e1831b32b3784394e3fb146521518
pavank0723 <pavan.yadav0723@gmail.com>
2022-12-11 18:54:22 +0530

    add middlewares

--- src/routes/achievements/certificateCategory.js
+++ src/routes/achievements/certificateCategory.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { certificateCategController } from "../../controllers";
+import { admin, auth } from "../../middlewares";
 
 var route = express.Router();
 
-route.post('/store', certificateCategController.store)
+route.post('/store',[auth, admin], certificateCategController.store)
 route.get('/views', certificateCategController.index)
 route.get('/view/:id', certificateCategController.show)
-route.put('/edit/:id', certificateCategController.edit)
-route.delete('/destroy/:id', certificateCategController.destroy)
+route.put('/edit/:id',[auth, admin], certificateCategController.edit)
+route.delete('/destroy/:id',[auth, admin], certificateCategController.destroy)
 
 export default route
--- src/routes/skill/skillCategory.js
+++ src/routes/skill/skillCategory.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { skillCategController } from "../../controllers";
+import { admin, auth } from "../../middlewares";
 
 var route = express.Router();
 
-route.post('/store',skillCategController.store)
+route.post('/store',[auth, admin],skillCategController.store)
 route.get('/views',skillCategController.show)
 route.get('/view/:id',skillCategController.index)
-route.put('/edit/:id',skillCategController.edit)
-route.delete('/destroy/:id',skillCategController.destroy)
+route.put('/edit/:id',[auth, admin],skillCategController.edit)
+route.delete('/destroy/:id',[auth, admin],skillCategController.destroy)
 
 export default route
