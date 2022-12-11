aea388d40097f5b822dfc8aa7e1c051bbe8b0cce
pavank0723 <pavan.yadav0723@gmail.com>
2022-12-11 18:57:21 +0530

    minor change

--- src/routes/skill/techSkill.js
+++ src/routes/skill/techSkill.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { techSkillController } from "../../controllers";
+import { admin, auth } from "../../middlewares";
 
 var route = express.Router();
 
-route.post('/store',techSkillController.store)
+route.post('/store',[auth, admin],techSkillController.store)
 route.get('/views',techSkillController.show)
 route.get('/view/:id',techSkillController.index)
-route.put('/edit/:id',techSkillController.edit)
-route.delete('/destroy/:id',techSkillController.destroy)
+route.put('/edit/:id',[auth, admin],techSkillController.edit)
+route.delete('/destroy/:id',[auth, admin],techSkillController.destroy)
 
 export default route
